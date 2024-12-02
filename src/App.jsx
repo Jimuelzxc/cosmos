6402b63e1ecbb95f00324807f4e123587e1098dd
Jimuelzxc <jimuelmarcgensaya@gmail.com>
2024-12-02 18:41:44 +0800

    zxc

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,13 @@
 import "./App.css";
-import Navbar from "./components/Navbar";
-import Chats from "./components/Chats/Chats";
-import Input from "./components/Input";
-import { useEffect, useRef, useState } from "react";
+import MainPage from "./Pages/MainPage";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
-  const [btnAvailable, setBtnAvailable] = useState(true);
-  const [firstMessage, setFirstMessage] = useState(true);
-  const [isLoad, setIsLoad] = useState(null);
-  const [messages, setMessages] = useState([
-    {
-      role: "system",
-      content:
-        "you are a chatbot. your name is cosmos. (speak in simple english)",
-    },
-    {
-      role: "assistant",
-      content: "Hi there, User can i help you with anything?",
-    },
-  ]);
-  const chatContainerRef = useRef(null);
-  useEffect(() => {
-    if (chatContainerRef.current) {
-      chatContainerRef.current.scrollTo({
-        top: chatContainerRef.current.scrollHeight,
-        behavior: "smooth",
-      });
-    }
-  }, [messages]);
   return (
     <>
-      <div className="h-screen w-full bg-black parent flex flex-col">
-        <Navbar />
-        <Chats
-          state={[messages, setMessages]}
-          firstMessage={firstMessage}
-          isLoad={isLoad}
-          chatContainerRef={chatContainerRef}
-        />
-        <Input
-          state={[messages, setMessages]}
-          setFirstMessage={setFirstMessage}
-          setIsLoad={setIsLoad}
-          btnState={[btnAvailable, setBtnAvailable]}
-        />
-      </div>
-
+      <Routes>
+        <Route exact path="/cosmos/" element={<MainPage />} />
+      </Routes>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { Routes, Route } from "react-router-dom";
 
 import "./index.css";
 import App from "./App.jsx";
@@ -9,9 +8,7 @@ import App from "./App.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </StrictMode>
 );
